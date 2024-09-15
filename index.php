df0f8191980564bfae1d368b363107860bf5ebd3
ThanhSangDev <workwithsang437@gmail.com>
2024-09-16 05:05:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile  -  thanhsangdev</title>
+    <meta name="author" content="ThanhSangDev">
+    <link rel="icon" href="thanhsangdev.jpg" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="px-4 py-8 mx-auto bg-white">
+        <div class="flex flex-col items-center min-h-screen font-mono">
+            <div class="w-full max-w-md overflow-hidden">
+                <div class="flex flex-col items-center p-8">
+                    <img src="thanhsangdev.jpg" alt="Avatar" class="rounded-full mb-6 border-4 border-gray-200 shadow-lg avatar" width="200" height="200">
+                    <h1 class="text-4xl font-bold mb-4 text-gray-800">THANHSANG-DEV</h1>
+                    <div class="flex flex-wrap justify-center gap-2 mb-4">
+                        <span class="bg-gray-200 px-3 py-1 rounded-full font-medium text-sm sm:text-base">👨‍💻 Web developer</span>
+                        <span class="bg-gray-200 px-3 py-1 rounded-full font-medium text-sm sm:text-base">🎹 Music</span>
+                        <span class="bg-gray-200 px-3 py-1 rounded-full font-medium text-sm sm:text-base">🎮 Dabble</span>
+                    </div>
+                    <div class="flex items-center mb-6 text-gray-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+                            <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
+                        </svg>
+                        <span>DakLak, VietNam</span>
+                    </div>
+                    <a href="https://github.com/dunkbing/dunkbing" target="_blank" class="bg-gray-800 text-white px-6 py-2 rounded-full mb-6 hover:bg-gray-700 transition-colors duration-300">README.md</a>
+                    <div class="flex space-x-4 mb-2">
+                        <a href="https://www.tiktok.com/@codezgit.com" target="_blank" class="w-12 h-12 bg-gray-200 rounded-full flex items-center justify-center transition-colors duration-300 hover:bg-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-tiktok" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M21 7.917v4.034a9.948 9.948 0 0 1 -5 -1.951v4.5a6.5 6.5 0 1 1 -8 -6.326v4.326a2.5 2.5 0 1 0 4 2v-11.5h4.083a6.005 6.005 0 0 0 4.917 4.917z"></path>
+                            </svg>
+                        </a>
+                        <a href="https://www.threads.net/@not" target="_blank" class="w-12 h-12 bg-gray-200 rounded-full flex items-center justify-center transition-colors duration-300 hover:bg-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-threads" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M19 7.5c-1.333 -3 -3.667 -4.5 -7 -4.5c-5 0 -8 2.5 -8 9s3.5 9 8 9s7 -3 7 -5s-1 -5 -7 -5c-2.5 0 -3 1.25 -3 2.5c0 1.5 1 2.5 2.5 2.5c2.5 0 3.5 -1.5 3.5 -5s-2 -4 -3 -4s-1.833 .333 -2.5 1"></path>
+                            </svg>
+                        </a>
+                        <a href="https://www.instagram.com/transang437/" target="_blank" class="w-12 h-12 bg-gray-200 rounded-full flex items-center justify-center transition-colors duration-300 hover:bg-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
+                                <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                            </svg>
+                        </a>
+                        <a href="https://github.com/transangca" target="_blank" class="w-12 h-12 bg-gray-200 rounded-full flex items-center justify-center transition-colors duration-300 hover:bg-gray-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="w-full">
+                        <h2 class="text-xl font-bold mb-4 text-gray-800 text-center">My Projects</h2>
+                        <a href="https://craweb.fun" target="_blank" class="bg-gray-100 rounded-lg p-4 flex justify-between items-center mb-2 hover:bg-gray-200">
+                            <div>
+                                <p class="text-lg font-semibold text-gray-800">Craweb.fun</p>
+                                <span class="text-sm text-gray-600">Dịch vụ đa nền tảng bao gồm game..</span>
+                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M9 15l6 -6"></path>
+                                <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464"></path>
+                                <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463"></path>
+                            </svg>
+                        </a>
+                        <a href="https://github.com/transangca/demngaysinhcuaban/blob/main/botdemngaysinh.py" target="_blank" class="bg-gray-100 rounded-lg p-4 flex justify-between items-center mb-2 hover:bg-gray-200">
+                            <div>
+                                <p class="text-lg font-semibold text-gray-800">Bot đếm ngày sinh</p>
+                                <span class="text-sm text-gray-600">Source code bot đếm ngày sinh thông qua telegrams</span>
+                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M9 15l6 -6"></path>
+                                <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464"></path>
+                                <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463"></path>
+                            </svg>
+                        </a>
+                        <a href="https://github.com/transangca/taoanhanime" target="_blank" class="bg-gray-100 rounded-lg p-4 flex justify-between items-center mb-2 hover:bg-gray-200">
+                            <div>
+                                <p class="text-lg font-semibold text-gray-800">Tạo ảnh anime</p>
+                                <span class="text-sm text-gray-600">tạo ảnh anime bằng bot telegrams random tạo ảnh</span>
+                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M9 15l6 -6"></path>
+                                <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464"></path>
+                                <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463"></path>
+                            </svg>
+                        </a>
+                        <a href="https://m33t.deno.dev" target="_blank" class="bg-gray-100 rounded-lg p-4 flex justify-between items-center mb-2 hover:bg-gray-200">
+                            <div>
+                                <p class="text-lg font-semibold text-gray-800">M33t</p>
+                                <span class="text-sm text-gray-600">A peer-to-peer, group video call web app, built with Deno and WebRTC.</span>
+                            </div>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M9 15l6 -6"></path>
+                                <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464"></path>
+                                <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"8c3b7c31ce60e6ca","version":"2024.8.0","r":1,"token":"248d112c1bed4af69c71d7296d75ad6c","serverTiming":{"name":{"cfExtPri":true,"cfL4":true}}}' crossorigin="anonymous"></script>
+<style>
+  /*! tailwindcss v3.4.1 | MIT License | https://tailwindcss.com */
+  *,:after,:before {
+    border: 0 solid #e5e7eb;
+    box-sizing: border-box;
+  }
+  :after,:before {
+    --tw-content: "";
+  }
+  html {
+    line-height: 1.5;
+    -webkit-text-size-adjust: 100%;
+    font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    font-feature-settings: normal;
+    font-variation-settings: normal;
+    -moz-tab-size: 4;
+    -o-tab-size: 4;
+    tab-size: 4;
+    -webkit-tap-highlight-color: transparent;
+  }
+  body {
+    line-height: inherit;
+    margin: 0;
+  }
+  hr {
+    border-top-width: 1px;
+    color: inherit;
+    height: 0;
+  }
+  abbr[title] {
+    -webkit-text-decoration: underline dotted;
+    text-decoration: underline dotted;
+  }
+  h1,h2,h3,h4,h5,h6 {
+    font-size: inherit;
+    font-weight: inherit;
+  }
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+  b,strong {
+    font-weight: bolder;
+  }
+  code,kbd,pre,samp {
+    font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace;
+    font-size: 1em;
+  }
+  small {
+    font-size: 80%;
+  }
+  sub,sup {
+    font-size: 75%;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
+  }
+  sub {
+    bottom: -.25em;
+  }
+  sup {
+    top: -.5em;
+  }
+  table {
+    border-collapse: collapse;
+    border-color: inherit;
+    text-indent: 0;
+  }
+  button,input,optgroup,select,textarea {
+    color: inherit;
+    font-family: inherit;
+    font-size: 100%;
+    line-height: inherit;
+    margin: 0;
+    padding: 0;
+  }
+  button,select {
+    text-transform: none;
+  }
+  [type=button],[type=reset],[type=submit],button {
+    -webkit-appearance: button;
+    background-color: transparent;
+    background-image: none;
+  }
+  :-moz-focusring {
+    outline: auto;
+  }
+  :-moz-ui-invalid {
+    box-shadow: none;
+  }
+  progress {
+    vertical-align: baseline;
+  }
+  ::-webkit-inner-spin-button, ::-webkit-outer-spin-button {
+    height: auto;
+  }
+  [type=search] {
+    -webkit-appearance: textfield;
+    outline-offset: -2px;
+  }
+  ::-webkit-search-decoration {
+    -webkit-appearance: none;
+  }
+  ::-webkit-file-upload-button {
+    -webkit-appearance: button;
+    font: inherit;
+  }
+  summary {
+    display: list-item;
+  }
+  blockquote,dd,dl,figure,h1,h2,h3,h4,h5,h6,hr,p,pre {
+    margin: 0;
+  }
+  fieldset {
+    margin: 0;
+    padding: 0;
+  }
+  menu,ol,ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  dialog {
+    padding: 0;
+  }
+  textarea {
+    resize: vertical;
+  }
+  input::-moz-placeholder,textarea::-moz-placeholder {
+    color: #9ca3af;
+    opacity: 1;
+  }
+  input::placeholder,textarea::placeholder {
+    color: #9ca3af;
+    opacity: 1;
+  }
+  [role=button],button {
+    cursor: pointer;
+  }
+  :disabled {
+    cursor: default;
+  }
+  audio,canvas,embed,iframe,img,object,svg,video {
+    display: block;
+    vertical-align: middle;
+  }
+  img,video {
+    height: auto;
+    max-width: 100%;
+  }
+  [hidden] {
+    display: none;
+  }
+  /* Tailwind Utility Classes */
+  .mx-auto { margin-left: auto; margin-right: auto; }
+  .my-4 { margin: 1rem 0; }
+  .my-6 { margin: 1.5rem 0; }
+  .mb-2 { margin-bottom: .5rem; }
+  .mb-4 { margin-bottom: 1rem; }
+  .mb-6 { margin-bottom: 1.5rem; }
+  .mr-2 { margin-right: .5rem; }
+  .flex { display: flex; }
+  .h-12 { height: 3rem; }
+  .h-6 { height: 1.5rem; }
+  .min-h-screen { min-height: 100vh; }
+  .w-12 { width: 3rem; }
+  .w-6 { width: 1.5rem; }
+  .w-full { width: 100%; }
+  .max-w-md { max-width: 28rem; }
+  .max-w-screen-md { max-width: 768px; }
+  .flex-col { flex-direction: column; }
+  .flex-wrap { flex-wrap: wrap; }
+  .items-center { align-items: center; }
+  .justify-center { justify-content: center; }
+  .justify-between { justify-content: space-between; }
+  .gap-2 { gap: .5rem; }
+  .gap-8 { gap: 2rem; }
+  .space-x-4 > :not([hidden]) ~ :not([hidden]) {
+    --tw-space-x-reverse: 0;
+    margin-left: calc(1rem * (1 - var(--tw-space-x-reverse)));
+    margin-right: calc(1rem * var(--tw-space-x-reverse));
+  }
+  .overflow-hidden { overflow: hidden; }
+  .rounded { border-radius: .25rem; }
+  .rounded-full { border-radius: 9999px; }
+  .rounded-lg { border-radius: .5rem; }
+  .border-2 { border-width: 2px; }
+  .border-4 { border-width: 4px; }
+  .border-gray-200 { border-color: rgb(229 231 235); }
+  .border-gray-500 { border-color: rgb(107 114 128); }
+  .bg-[#86efac] { background-color: rgb(134 239 172); }
+  .bg-gray-100 { background-color: rgb(243 244 246); }
+  .bg-gray-200 { background-color: rgb(229 231 235); }
+  .bg-gray-800 { background-color: rgb(31 41 55); }
+  .bg-white { background-color: rgb(255 255 255); }
+  .p-4 { padding: 1rem; }
+  .p-8 { padding: 2rem; }
+  .px-2 { padding-left: .5rem; padding-right: .5rem; }
+  .px-3 { padding-left: .75rem; padding-right: .75rem; }
+  .px-4 { padding-left: 1rem; padding-right: 1rem; }
+  .px-6 { padding-left: 1.5rem; padding-right: 1.5rem; }
+  .py-1 { padding-top: .25rem; padding-bottom: .25rem; }
+  .py-2 { padding-top: .5rem; padding-bottom: .5rem; }
+  .py-6 { padding-top: 1.5rem; padding-bottom: 1.5rem; }
+  .py-8 { padding-top: 2rem; padding-bottom: 2rem; }
+  .text-center { text-align: center; }
+  .font-mono { font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace; }
+  .text-3xl { font-size: 1.875rem; line-height: 2.25rem; }
+  .text-4xl { font-size: 2.25rem; line-height: 2.5rem; }
+  .text-lg { font-size: 1.125rem; line-height: 1.75rem; }
+  .text-sm { font-size: .875rem; line-height: 1.25rem; }
+  .text-xl { font-size: 1.25rem; line-height: 1.75rem; }
+  .font-bold { font-weight: 700; }
+  .font-medium { font-weight: 500; }
+  .font-semibold { font-weight: 600; }
+  .tabular-nums { font-variant-numeric: tabular-nums; }
+  .text-gray-500 { color: rgb(107 114 128); }
+  .text-gray-600 { color: rgb(75 85 99); }
+  .text-gray-800 { color: rgb(31 41 55); }
+  .text-white { color: rgb(255 255 255); }
+  .underline { text-decoration-line: underline; }
+  .shadow-lg { box-shadow: 0 10px 15px -3px rgba(0,0,0,.1), 0 4px 6px -4px rgba(0,0,0,.1); }
+  .transition-colors { transition: color .15s, background-color .15s, border-color .15s, text-decoration-color .15s, fill .15s, stroke .15s; }
+  .duration-300 { transition-duration: .3s; }
+  .hover\:bg-gray-200:hover { background-color: rgb(229 231 235); }
+  .hover\:bg-gray-300:hover { background-color: rgb(209 213 219); }
+  .hover\:bg-gray-700:hover { background-color: rgb(55 65 81); }
+  @media (min-width: 640px) {
+    .sm\:text-base { font-size: 1rem; line-height: 1.5rem; }
+  }
+   .avatar {
+    @apply w-16 h-16 rounded-full border-2 border-gray-300 transition-transform duration-300;
+  }
+  .avatar:hover {
+    @apply transform rotate-12 shadow-lg;
+  }
+</style>
+
+</body></html>
